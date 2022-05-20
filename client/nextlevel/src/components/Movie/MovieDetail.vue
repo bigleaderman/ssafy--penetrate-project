ffdac20a7c390de0aae6a11db3c73f8d404ec58f
허건녕 <heo3793@naver.com>
2022-05-21 02:07:31 +0900

    modal

--- client/nextlevel/src/components/Movie/MovieDetail.vue
+++ client/nextlevel/src/components/Movie/MovieDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="detail" class="container-fluid" :style="{'background-image':`url('http://image.tmdb.org/t/p/w185${moviedetail.backdrop_path}')`}">
+  <div id="detail" class="container-fluid" :style="{'background-image':`url('http://image.tmdb.org/t/p/w500${moviedetail.backdrop_path}')`}">
     <div id="detail2" >
       <h1>{{ moviedetail.title }}</h1>
       
@@ -23,6 +23,7 @@ export default {
   z-index: 30;
   width: 100%;
   height: 1000px;
+  padding: 2.5%;
   margin: 0;
   background-repeat: no-repeat;
   background-size: cover;
