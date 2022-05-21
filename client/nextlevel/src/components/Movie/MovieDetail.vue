518bdc9f8fa780d91d53f7f3c70ff88402162003
허건녕 <heo3793@naver.com>
2022-05-21 15:29:17 +0900

    modal

--- client/nextlevel/src/components/Movie/MovieDetail.vue
+++ client/nextlevel/src/components/Movie/MovieDetail.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="detail" class="container-fluid" :style="{'background-image':`url('http://image.tmdb.org/t/p/w500${moviedetail.backdrop_path}')`}">
     <div id="detail2" >
-      <h1>{{ moviedetail.title }}</h1>
-      
+      <div class="row">
+        <div id="youtube" class="modal-div col col-xs-12 col-md-6">
+          
+        </div>
+        <div id="contents" class="col col-xs-12 col-md-6">
+          <h1>{{ moviedetail.title }}</h1>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,16 +28,21 @@ export default {
 #detail{
   z-index: 30;
   width: 100%;
-  height: 1000px;
-  padding: 2.5%;
+  height: auto;
+  padding: 20px;
   margin: 0;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 }
-
 #detail2{
   z-index: 40;
+  height: 1200px ;
+  padding: 3%;
+  background-color: rgba(0, 0, 0, 0.667);
 }
-
+.modal-div{
+  background-color:rgb(110, 65, 65);
+  height: 600px;
+}
 </style>
